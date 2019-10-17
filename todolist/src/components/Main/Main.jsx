178c730e26c4fdd5f7c3e3fc1fa9294f2ecb29ea
言志伟 <yanzhiwei@encootech.com>
2019-10-17 15:06:50 +0800

    step1

---
+++ todolist/src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import  TopBlock  from '../TopBlock/TopBlock';
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <TopBlock />
+            </div>
+        )
+    }
+}
---
+++ todolist/src/components/TopBlock/TopBlock.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TopBlock extends Component {
+    render() {
+        return (
+            <div id="topBlock">
+
+            </div>
+        )
+    }
+}
