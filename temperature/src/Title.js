639fe5802936b8aa45562db3aa0ac1ba0af2900c
言志伟 <yanzhiwei@encootech.com>
2019-10-17 09:55:07 +0800

    delete

--- temperature/src/Title.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Title extends Component {
-    render() {
-        return <h1>{(this.props.temperature >= 100 ? '水已经沸了' : '水未开...')}</h1>
-    }
-}
