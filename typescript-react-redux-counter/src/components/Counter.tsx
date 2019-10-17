7ee3b0cb61d58610831296ae12aa947f5f957072
言志伟 <yanzhiwei@encootech.com>
2019-10-17 19:52:06 +0800

    remark

--- typescript-react-redux-counter/src/components/Counter.tsx
+++ typescript-react-redux-counter/src/components/Counter.tsx
@@ -11,6 +11,10 @@ export interface IProps {
 export default class Counter extends React.PureComponent<IProps> {
     public render() {
         const { value, onIncrement, onDecrement } = this.props;
+        //等同于
+        //const value=this.props.value;
+        //const onIncrement=this.props.onIncrement;
+        //const onDecrement=this.props.onDecrement;
         return (
             <p>
                 Clicked: {value} times
