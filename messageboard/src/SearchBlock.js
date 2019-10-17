85e9c41a54ee9555a3791bca76ffccd8fb772c64
言志伟 <yanzhiwei@encootech.com>
2019-10-17 11:20:31 +0800

    create project

---
+++ messageboard/src/SearchBlock.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchBlock extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
