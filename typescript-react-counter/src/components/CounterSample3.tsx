2b8da84d66ac8f46fb5fdd82160b40f828df09b8
言志伟 <yanzhiwei@encootech.com>
2019-11-22 17:12:09 +0800

    add HelloWorld

---
+++ typescript-react-counter/src/components/CounterSample3.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CounterSample3 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
